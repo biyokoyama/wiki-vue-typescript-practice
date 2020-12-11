89d61c98948f5541811a485ce839329c2d783f0e
biyokoyama <biyokoyamajp@gmail.com>
2020-12-12 01:04:39 +0900

    axios-mock-server の初期設定

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "./main/axios-mock-server.ts";
+
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ src/main/axios-mock-server.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore: Cannot find module
+import mock from "../../mocks/$mock.js";
+if (process.env.NODE_ENV === "development") mock();
