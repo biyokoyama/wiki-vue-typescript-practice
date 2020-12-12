4a5edd0d0a32ce0300810f644cf261f497e4df77
biyokoyama <biyokoyamajp@gmail.com>
2020-12-12 22:00:38 +0900

    fontawesome fab 導入
    yarn add \
    @fortawesome/vue-fontawesome@latest \
    @fortawesome/fontawesome-svg-core \
    @fortawesome/free-brands-svg-icons

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "./main/fontawesome.ts";
 import "./main/axios-mock-server.ts";
 
 Vue.config.productionTip = false;
