610d9b2aa92a3c4bb7e7cf9d8f4a13b4af5056ba
biyokoyama <biyokoyamajp@gmail.com>
2020-12-12 22:08:23 +0900

    Footer：Githubのリンク追加

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,22 @@
 <template>
   <footer class="footer has-background-light">
-    <div class="content has-text-centered">
-      <p>©︎2020 biyokoyama</p>
+    <div class="field is-grouped is-grouped-centered">
+      <p class="control">
+        ©︎2020 biyokoyama
+      </p>
+      <p class="control">
+        <a
+          href="https://github.com/biyokoyama/wiki-vue-typescript-practice"
+          target="_blank"
+          rel="noopener"
+          class="button is-small"
+        >
+          <FontAwesomeIcon class="icon" :icon="['fab', 'github']" />
+          <span>
+            Github
+          </span>
+        </a>
+      </p>
     </div>
   </footer>
 </template>
